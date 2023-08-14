eb8864fe558f36e1c9c4c6eb2788d52ee4c985b8
anisiocmjr <anisiocmjr@gmail.com>
2023-08-14 11:03:08 -0300

    Estudo com loops e arrays.

---
+++ EstruturasCondicionais/src/ForEstudo.java
@@ -0,0 +1,7 @@
+public class ForEstudo {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 30; carneirinhos ++){
+            System.out.println("Contando Carneirinhos " + carneirinhos);
+        }
+    }
+}
---
+++ EstruturasCondicionais/src/ForWithArrays.java
@@ -0,0 +1,8 @@
+public class ForWithArrays {
+    public static void main(String[] args) {
+        String alunos [] = {"Jonas", "Marcos", "Paulo", "Rebecca"};
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno: " + aluno);
+        }
+    }
+}
